a77b61d252fea1559bf81177f148059581103444
mmistroni <mmistroni@gmail.com>
2017-12-19 22:07:03 +0000

    Added code to find best decision tree

--- src/main/scala/ml/DecisionTreeLoader.scala
+++ src/main/scala/ml/DecisionTreeLoader.scala
@@ -17,6 +17,7 @@ import org.apache.spark.ml.tuning.{ CrossValidator, ParamGridBuilder }
 import org.apache.spark.ml.tuning.{ ParamGridBuilder, TrainValidationSplit }
 import org.apache.spark.ml.{ Pipeline, PipelineModel }
 import org.apache.spark.ml.feature._
+import scala.util.Random
 
 /**
  *   Use a DecisionTree Classifier to generate predictions on DataFrame
@@ -91,9 +92,52 @@ class DecisionTreeLoader(label: String, dataSplit: Array[Double] = Array(0.7, 0.
     val treeModel = model.stages(2).asInstanceOf[DecisionTreeClassificationModel]
     println("Learned classification tree model:\n" + treeModel.toDebugString)
     
-    
+    findBestModel(dt, evaluator, pipeline, trainingData, testData)
     
   }
+  
+  private def findBestModel(classifier:DecisionTreeClassifier,
+                            multiclassEval:MulticlassClassificationEvaluator,
+                            pipeline:Pipeline,
+                            trainData:DataFrame,
+                            testData:DataFrame):Unit = {
+    println("######### FINDING BEST MODEL-------------")
+    val paramGrid = new ParamGridBuilder().
+      addGrid(classifier.impurity, Seq("gini", "entropy")).
+      addGrid(classifier.maxDepth, Seq(1, 20)).
+      addGrid(classifier.maxBins, Seq(40, 300)).
+      addGrid(classifier.minInfoGain, Seq(0.0, 0.05)).
+      build()
+
+    
+    val validator = new TrainValidationSplit().
+      setSeed(Random.nextLong()).
+      setEstimator(pipeline).
+      setEvaluator(multiclassEval).
+      setEstimatorParamMaps(paramGrid).
+      setTrainRatio(0.9)
+
+    val validatorModel = validator.fit(trainData)
+    val bestModel = validatorModel.bestModel
+    logger.info("============== BEST MODEL ")
+    logger.info(bestModel.asInstanceOf[PipelineModel].stages.last.extractParamMap)
+
+    logger.info("============== VALIDATION METRICS ")
+    logger.info(validatorModel.validationMetrics.max)
+
+    val testAccuracy = multiclassEval.evaluate(bestModel.transform(testData))
+    logger.info("============== TEST ACCURACY MODEL ")
+    logger.info(testAccuracy)
+    val trainAccuracy = multiclassEval.evaluate(bestModel.transform(trainData))
+    logger.info("============== TRAIN ACCURACY MODEL ")
+
+    logger.info(trainAccuracy)
+
+    (bestModel.asInstanceOf[PipelineModel], trainAccuracy, testAccuracy)
+
+  }
+  
+  
   
   
 
